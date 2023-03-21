d100bbe477ee1fe839dedf2eaf102cb3e7500c93
droysky <droysky@mail.com>
2023-03-21 21:04:19 +0700

    add 13 done

---
+++ 13. Roman to Integer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class Solution {
+ public:
+  int romanToInt(string str) {
+    map<char, int> dictionary = {{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+                                 {'C', 100}, {'D', 500}, {'M', 1000}};
+
+    int result = 0;
+    for (int i = 0; i < static_cast<int>(str.length()); i++) {
+      if (dictionary[str[i + 1]] <= dictionary[str[i]]) {
+        result += dictionary[str[i]];
+      } else {
+        result -= dictionary[str[i]];
+      }
+    }
+    return result;
+  }
+};
+
+int main() {
+  Solution s;
+
+  cout << s.romanToInt("LVIII") << endl;
+  cout << s.romanToInt("MCMXCIV") << endl;
+  return 0;
+}
